c7b96e32cfa76407874be5d5aabaf5ba57890c51
SeuNome <marioestimahackz@gmail.com>
2026-03-02 14:48:49 +0100

    feat: setup PWA com tradução automática e PDF

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,21 @@
 const cacheName = "news-pwa-v1";
 const filesToCache = [
+  "/",
   "/index.html",
-  "/css/styles.css",
+  "/styles/styles.css",
   "/js/app.js",
-  "/js/pdf.js",
-  "/manifest.json",
-];
+  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css",
+];c
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(filesToCache);
-    })
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
   );
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((resp) => {
-      return resp || fetch(e.request);
-    })
+    caches.match(e.request).then((resp) => resp || fetch(e.request))
   );
 });
